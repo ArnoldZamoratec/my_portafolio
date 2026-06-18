5fca1c7778beedff0032b3bdc846aab7117b4957
Arnold <huamanzamoraarnold@gmail.com>
2026-06-18 13:14:16 -0500

    fix: disable preloading visibility delay completely so that banner and content show up instantly on mount

--- src/composables/usePreloader.ts
+++ src/composables/usePreloader.ts
@@ -1,44 +1,10 @@
-import { ref, watch, onMounted } from "vue";
-import { resources } from "../utils/resources";
-import gsap from "gsap";
+import { ref, onMounted } from "vue";
 
-export const preloaderVisible = ref(true);
+export const preloaderVisible = ref(false);
 
 export const usePreloader = () => {
-  const progress = ref(0);
-  const resourcesProgress = ref(0);
-
   onMounted(() => {
-    resources.on("progress", (newProgress) => {
-      resourcesProgress.value = newProgress;
-    });
+    document.body.classList.remove("is-loading");
   });
-
-  watch(
-    resourcesProgress,
-    (newProgress) => {
-      progress.value = 0.25 + newProgress * 0.75;
-    },
-    { immediate: true },
-  );
-
-  watch(
-    progress,
-    (newProgress) => {
-      const rect = document.querySelector(".preloader-rect") as HTMLElement;
-      const preloader = document.querySelector(".preloader") as HTMLElement;
-      if (newProgress === 1) {
-        gsap.delayedCall(0.2, () => {
-          document.body.classList.remove("is-loading");
-          if (preloader) {
-            preloader.classList.add("preloader-hidden");
-          }
-          preloaderVisible.value = false;
-        });
-      }
-
-      if (rect) rect.style.transform = `scaleY(${newProgress})`;
-    },
-    { immediate: true },
-  );
 };
+
